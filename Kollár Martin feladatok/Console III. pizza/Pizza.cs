5a9dd6e856f3b4fd6ec43f85a7b12a1b2e44f3af
kollarm02 <114418339+kollarm02@users.noreply.github.com>
2024-05-16 17:03:29 +0200

    Add files via upload

---
+++ Kollár Martin feladatok/Console III. pizza/Pizza.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pizza
+{
+    internal class Pizza
+    {
+        public int sorszam;
+        public string nev;
+        public string pizza;
+        public int ar;
+        public string csaladi;
+        public int db;
+
+        public Pizza(string sorszam, string nev, string pizza, string ar, string csaladi, string db)
+        {
+            this.sorszam =int.Parse(sorszam);
+            this.nev = nev;
+            this.pizza = pizza;
+            this.ar = int.Parse(ar);
+            this.csaladi = csaladi;
+            this.db = int.Parse(db);
+        }
+    }
+}
