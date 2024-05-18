9523c7cae2bf6b7b5f6006cb10df00a34e9619ec
kollarm02 <114418339+kollarm02@users.noreply.github.com>
2024-05-18 18:43:06 +0200

    Add files via upload

---
+++ Kollár Martin feladatok/Console Rendezveny/Rendezveny.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.feladat
+{
+    internal class Rendezveny
+    {
+        public int sorszam;
+        public string nev;
+        public string helyszin;
+        public int fo;
+        public int nap;
+        public int fopernap;
+        public string kedvezmenye;
+
+        public Rendezveny(string sorszam, string nev, string helyszin, string fo, string nap, string fopernap, string kedvezmenye)
+        {
+            this.sorszam = int.Parse(sorszam);
+            this.nev = nev;
+            this.helyszin = helyszin;
+            this.fo = int.Parse(fo);
+            this.nap = int.Parse(nap);
+            this.fopernap = int.Parse(fopernap);
+            this.kedvezmenye = kedvezmenye;
+        }
+    }
+}
